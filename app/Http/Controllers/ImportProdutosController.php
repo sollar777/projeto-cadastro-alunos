ba229d2ee159f1268aa04eac7c5abb972907690b
Jorge Ferreira <sollar777@gmail.com>
2021-03-04 20:36:35 -0300

    ajuste do facade para importar o arquivo csv

--- app/Http/Controllers/ImportProdutosController.php
+++ app/Http/Controllers/ImportProdutosController.php
@@ -2,13 +2,9 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Produto;
-use App\Models\Turma;
-use App\Models\Turma_Produto;
-use Carbon\CarbonImmutable;
+use App\Facedes\Utilities;
 use Illuminate\Http\Request;
-use League\Csv\Reader;
-use League\Csv\Statement;
+
 
 class ImportProdutosController extends Controller
 {
@@ -19,78 +15,17 @@ class ImportProdutosController extends Controller
 
     public function uploadCsv(Request $request)
     {
-        if ($request->csv->isValid()) {
-            //dd($request->csv->extension()); pega a extensao
-            // $teste = explode(".", $request->csv->getClientOriginalName());
-            // return $teste;
-            //dd($request->csv->getClientOriginalName());
-            $file = $request->csv->storeAs('csv', 'produtos' . CarbonImmutable::now()->isoFormat('DD-MM-YYYY') . '.csv');
-        }
+        $dados = $request;
+        $message = "";
 
+        if (Utilities::uploadProdutos($dados)){
+            $message = "Tudo Ok";
+        }else{
+            $message = "Error";
+        }
         
 
-        $csv = Reader::createFromPath('../storage/app/' . $file, 'r');
-        $csv->setDelimiter(";");
-        $headers = $csv->setHeaderOffset(0);
-
-        $stmt = (new Statement())->offset(0);
-
-        $records = $stmt->process($csv);
-        $headers = $records->getHeader();
-        $produtos = [];
-
-        $turmas_produtos = Turma_Produto::all();
-
-        foreach ($records as $key => $record) {
-            $produtos[$key] = array_values($record);
-        }
-
-        for ($i = 3; $i < count($headers); $i++) {
-            $turma = Turma::where("nome", $headers[$i])->first();
-            if (!$turma) {
-                if ($headers[$i] !== "" || !isset($headers[$i])) {
-                    Turma::create([
-                        'nome' => $headers[$i]
-                    ]);
-                }
-            }
-        }
-
-        foreach ($produtos as $produto) {
-            $mercadoria = Produto::where("nome", $produto[0])->first();
-            if (!$mercadoria) {
-                $mercadoria = Produto::create([
-                    "nome" => $produto[0],
-                    "preco" => $produto[1],
-                    "estoque" => $produto[2]
-                ]);
-            }
-        }
-
-        $turmas = Turma::all();
-
-        foreach($produtos as $produto){
-            for ($i = 3; $i < count($produto) - 1; $i++) {
-                if ($produto[$i] === "x") {
-                    $turma = $turmas->where('nome', $headers[$i])->first();
-                    $mercadoria = Produto::where('nome', $produto[0])->first();
-                    if (count($turmas_produtos) == 0) {
-                        $ligacao = Turma_Produto::create([
-                            "turma_id" => $turma->id,
-                            "produto_id" => $mercadoria->id
-                        ]);
-                    } else {
-                        $produtos_turmas = $turma->produtos()->where("produto_id", $mercadoria->id)->get();
-                        if (!$produtos_turmas) {
-                            $ligacao = Turma_Produto::create([
-                                "turma_id" => $turma->id,
-                                "produto_id" => $mercadoria->id
-                            ]);
-                        }
-                    }
-                }
-            }
-        }
+        return Response()->json([$message],200);
     }
 
     //faltando fazer o model dos controoler pra savar os produtos
--- app/Services/Utilities.php
+++ app/Services/Utilities.php
@@ -2,6 +2,14 @@
 
 namespace App\Services;
 
+use App\Models\Produto;
+use App\Models\Turma;
+use App\Models\Turma_Produto;
+use Carbon\CarbonImmutable;
+use GuzzleHttp\Psr7\Request;
+use League\Csv\Reader;
+use League\Csv\Statement;
+
 class Utilities
 {
     public function getCep($cep)
@@ -13,4 +21,85 @@ class Utilities
 
         return $retorno;
     }
+
+    public function uploadProdutos($dados)
+    {
+        if ($dados->csv->isValid()) {
+            //dd($request->csv->extension()); pega a extensao
+            // $teste = explode(".", $request->csv->getClientOriginalName());
+            // return $teste;
+            //dd($request->csv->getClientOriginalName());
+            $file = $dados->csv->storeAs('csv', 'produtos' . CarbonImmutable::now()->isoFormat('DD-MM-YYYY') . '.csv');
+        }
+
+        
+
+        $csv = Reader::createFromPath('../storage/app/' . $file, 'r');
+        $csv->setDelimiter(";");
+        $headers = $csv->setHeaderOffset(0);
+
+        $stmt = (new Statement())->offset(0);
+
+        $records = $stmt->process($csv);
+        $headers = $records->getHeader();
+        $produtos = [];
+
+        $turmas_produtos = Turma_Produto::all();
+
+        foreach ($records as $key => $record) {
+            $produtos[$key] = array_values($record);
+        }
+
+        //cadastrando as turmas
+        for ($i = 3; $i < count($headers); $i++) {
+            $turma = Turma::where("nome", $headers[$i])->first();
+            if (!$turma) {
+                if ($headers[$i] !== "" || !isset($headers[$i])) {
+                    Turma::create([
+                        'nome' => $headers[$i]
+                    ]);
+                }
+            }
+        }
+
+        foreach ($produtos as $produto) {
+            $mercadoria = Produto::where("nome", $produto[0])->first();
+            if (!$mercadoria) {
+                $mercadoria = Produto::create([
+                    "nome" => $produto[0],
+                    "preco" => $produto[1],
+                    "estoque" => $produto[2]
+                ]);
+            }
+        }
+
+        $turmas = Turma::all();
+
+        foreach($produtos as $produto){
+            for ($i = 3; $i < count($produto) - 1; $i++) {
+                if ($produto[$i] === "x") {
+                    $turma = $turmas->where('nome', $headers[$i])->first();
+                    $mercadoria = Produto::where('nome', $produto[0])->first();
+                    if (count($turmas_produtos) == 0) {
+                        $ligacao = Turma_Produto::create([
+                            "turma_id" => $turma->id,
+                            "produto_id" => $mercadoria->id
+                        ]);
+                    } else {
+                        $produtos_turmas = $turma->produtos()->where("produto_id", $mercadoria->id)->get();
+                        if (!$produtos_turmas) {
+                            $ligacao = Turma_Produto::create([
+                                "turma_id" => $turma->id,
+                                "produto_id" => $mercadoria->id
+                            ]);
+                        }
+                    }
+                }
+            }
+        }
+
+        return true;
+
+    }
+
 }
